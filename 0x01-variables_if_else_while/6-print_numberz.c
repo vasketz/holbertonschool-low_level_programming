dd4a20df9579afdfa0806bed756659c0b4669e38
vasketz <anfevach@gmail.com>
2021-02-05 00:26:14 +0000

    update file 6-print-numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point and execute the numbers with putchar
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
