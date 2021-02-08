ea6e78b048720e176eb0e94347d3146dce0a9d13
vasketz <anfevach@gmail.com>
2021-02-08 23:06:57 +0000

    Update header .h

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,2 +1,13 @@
+/**
+ * Print function
+ */
 int _putchar(char c);
-int print_alphabet(void);
+
+/**
+ * This function prints the alphabet
+ */
+void print_alphabet(void);
+/**
+ * This function prints alphabet x10
+ */
+void print_alphabet_x10(void);
