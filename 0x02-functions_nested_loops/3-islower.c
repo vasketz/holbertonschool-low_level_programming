83973de6c4d4408fd3af33ce0dee5ce9a721c86b
vasketz <anfevach@gmail.com>
2021-02-09 03:09:14 +0000

    Update file 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <ctype.h>
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+
+	if (c == islower(c))
+	{
+		return(1);
+	}
+	else
+	{
+		return(0);
+	}
+	return (0);
+}
