42df20f5715334177c5338107e397f71bffb7b4e
vasketz <anfevach@gmail.com>
2021-02-09 16:08:56 +0000

    Update file 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_sign - prints the sign of a number
+ *@n: is a variable
+ * Return: Always 0.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		return (0);
+	}
+}
