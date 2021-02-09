fc0c0d880e56856fcd687bda49104c326c4728d6
vasketz <anfevach@gmail.com>
2021-02-09 18:28:59 +0000

    Update file 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - print the hours and minutes.
+ *
+ * Return: Always 0.
+ */
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h < 23; h++)
+		for (m = 0; m < 59; m++)
+		{
+			_putchar((h / 10) + 48);
+			_putchar((h % 10) + 48);
+			_putchar(58);
+			_putchar((m / 10) + 48);
+			_putchar((m % 10) + 48);
+			_putchar('\n');
+		}
+}
