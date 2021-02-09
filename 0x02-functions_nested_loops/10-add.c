b5b7fae8781c563b9f5de89d6e37258d7bebfe11
vasketz <anfevach@gmail.com>
2021-02-09 23:30:37 +0000

    update file 10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
+ * add - check the code for Holberton School students.
+ *@n1: is a variable
+ *@n2: is a variable
  * Return: Always 0.
  */
 int add(int n1, int n2)
 {
 	int sum;
+
 	sum = (n1 + n2);
+
 	return (sum);
 }
