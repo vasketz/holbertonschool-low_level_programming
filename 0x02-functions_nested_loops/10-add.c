fff87ac637f096c1172ec2a8881f5413c0422e74
vasketz <anfevach@gmail.com>
2021-02-09 23:27:00 +0000

    update file 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int add(void)
+{
+	int n1 = 0;
+	int n2 = 0;
+	int sum;
+	sum = (n1 + n2);
+	return (sum);
+}
