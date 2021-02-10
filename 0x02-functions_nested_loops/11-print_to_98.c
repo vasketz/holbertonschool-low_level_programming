1466b807c927b7924d2a510dd67c7cd9298d676b
vasketz <anfevach@gmail.com>
2021-02-10 01:49:17 +0000

    update file 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,13 +7,15 @@
  */
 void print_to_98(int n)
 {
-	for (; n <= 98; n++)
+	for (; n < 98; n++)
 	{
 		printf("%d, ", n);
 	}
-	for (; n >= 98; n--)
+	for (; n > 98; n--)
 	{
 		printf("%d, ", n);
 	}
+	if (n == 98)
+		printf("%d", n);
 	printf("\n");
 }
