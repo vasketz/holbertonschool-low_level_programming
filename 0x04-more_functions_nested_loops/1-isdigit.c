7cb8940934e30d55198974e08ae9718c29d415f5
vasketz <anfevach@gmail.com>
2021-02-11 17:24:37 +0000

    Update file 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _isupper - check the code for Holberton School students.
+ *@c: is a variable
+ * Return: Always 0.
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+	{
+		return (1);
+	}
+	else if (c != '0' || c != '9')
+	{
+		return (0);
+	}
+	return (0);
+}
