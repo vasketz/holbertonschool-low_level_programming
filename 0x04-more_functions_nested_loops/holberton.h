3aee761a24d68883b398cf0188b5eedc4a366db3
vasketz <anfevach@gmail.com>
2021-02-11 17:53:29 +0000

    update header .h

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,4 +1,8 @@
 /**
+ * Function to print with putchar
+ */
+int _putchar(char c);
+/**
  *This function checks if its a uppercase
  */
 int _isupper(int c);
@@ -10,3 +14,7 @@ int _isdigit(int c);
  *This function multiply two integers
  */
 int mul(int a, int b);
+/**
+ *This function prints the numbers from 0 to 9
+ */
+void print_numbers(void);
