deecb38ac34f959a504b3d304bef28f48691c2da
vasketz <anfevach@gmail.com>
2021-02-11 17:01:17 +0000

    update file 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int _isupper(int c)
+{
+	if (c == 'A')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
