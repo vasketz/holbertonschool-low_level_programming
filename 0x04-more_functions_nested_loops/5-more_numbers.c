8a0901932a88a6392dbed0dd969bd03b8f480ee0
vasketz <anfevach@gmail.com>
2021-02-11 19:09:36 +0000

    update file 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	int n;
+	int i;
+
+	for (n = 0; n <= 9; n++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i > 10)
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
