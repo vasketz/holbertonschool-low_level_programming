37acc8998cc86f66abe3235871e7130454e1298c
vasketz <anfevach@gmail.com>
2021-02-12 01:47:52 +0000

    Update file 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_square - check the code for Holberton School students.
+ *@size: is a variable
+ * Return: Always 0.
+ */
+void print_square(int size)
+{
+	int col;
+	int fila;
+
+	for (col = 0; col < size; col++)
+	{
+		for (fila = 0; fila < size; fila++)
+		{
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+}
