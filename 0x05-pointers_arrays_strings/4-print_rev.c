7d5af7ba843d73bc673456ca83d7fa40e44f4bae
vasketz <anfevach@gmail.com>
2021-02-16 22:17:42 +0000

    Update file 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,17 +1,16 @@
 #include "holberton.h"
 #include <string.h>
 /**
- * main - check the code for Holberton School students.
- *
+ * print_rev - check the code for Holberton School students.
+ *@s: is a pointer variable
  * Return: Always 0.
  */
 void print_rev(char *s)
 {
 	int i;
 
-	for (i = *s; i >= 0; i-- )
+	for (i = *s - 1; i >= 0; i-- )
 	{
-		if (s[i] != '\0')
 		_putchar(s[i]);
 	} 
 	_putchar('\n');
