489d17b02c82329d4d4f1f49e32cc67b98f4a0de
vasketz <anfevach@gmail.com>
2021-02-16 23:22:51 +0000

    Update file 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,13 +9,9 @@ void print_rev(char *s)
 {
 	int n;
 
-	for (n = 0; s[n] != '\0'; n++)
+	for (n = *s; n >= '\0'; --n)
 	{
-	}
-
-	while (n >= 0)
-	{
-		_putchar(s[--n]);
+		_putchar(s[n]);
 	}
 	_putchar('\n');
 }
