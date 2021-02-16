3aa1ee4311fb5edeafd3c8aaa3a1ace0d8d39809
vasketz <anfevach@gmail.com>
2021-02-16 20:06:08 +0000

    Update file 0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - check the code for Holberton School students.
+ *@n: is a pointer variable
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
