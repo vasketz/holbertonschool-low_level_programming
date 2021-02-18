4e2ab549ae33e2aafd7805f3610a9e172980ea22
vasketz <anfevach@gmail.com>
2021-02-18 22:17:54 +0000

    Update header holberton.h

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -10,3 +10,7 @@ char *_strncat(char *dest, char*src, int n);
  *This function copy a string
  */
 char *_strncpy(char *dest, char *src, int n);
+/**
+ *This function compares 2 strings
+ */
+int _strcmp(char *s1, char *s2);
