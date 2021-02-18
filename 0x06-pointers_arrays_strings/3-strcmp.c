a4b40e0058529d9a50819a9be7eb185efb993b39
vasketz <anfevach@gmail.com>
2021-02-18 22:19:34 +0000

    update file 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
+ * _strcmp - check the code for Holberton School students.
+ *@s1: is a pointer variable
+ *@s2: is a pointer variable
  * Return: Always 0.
  */
 int _strcmp(char *s1, char *s2)
@@ -14,7 +15,7 @@ int _strcmp(char *s1, char *s2)
 		s2++;
 	}
 
-	if(*s1 == *s2)
+	if (*s1 == *s2)
 	{
 		return (0);
 	}
