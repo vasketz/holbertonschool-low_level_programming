9f501baa431a9f004f3689a09ca79bc6887b64ea
vasketz <anfevach@gmail.com>
2021-02-18 21:53:22 +0000

    Update header.h

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -6,3 +6,7 @@ char *_strcat(char *dest, char *src);
  *This function concatenates to strings
  */
 char *_strncat(char *dest, char*src, int n);
+/**
+ *This function copy a string
+ */
+char *_strncpy(char *dest, char *src, int n);
