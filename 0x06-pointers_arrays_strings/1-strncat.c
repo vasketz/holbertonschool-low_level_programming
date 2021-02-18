3f3f41667025debdd6d498a7f7aeb625b02b87ab
vasketz <anfevach@gmail.com>
2021-02-18 18:17:36 +0000

    Update file 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strncat - check the code for Holberton School students.
+ *@dest: is a pointer variable
+ *@src: is a pointer variable
+ *@n: is a variable
+ * Return: Always 0.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	char *start = dest;
+	int x = 0;
+	int y = 0;
+
+	while (dest[x] != '\0')
+	{
+		x++;
+	}
+
+	while (src[y] && y < n)
+	{
+		dest[x + y] = src[y];
+		dest++;
+		src++;
+	}
+
+	*dest = '\0';
+	return (start);
+}
