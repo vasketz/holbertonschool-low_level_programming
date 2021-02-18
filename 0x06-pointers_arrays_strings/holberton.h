b26317a2616de1625f193492c3a1930ba9119613
vasketz <anfevach@gmail.com>
2021-02-18 18:17:09 +0000

    Update header holberton.h

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -2,3 +2,7 @@
  *This function concatenates to variables
  */
 char *_strcat(char *dest, char *src);
+/**
+ *This function concatenates to strings
+ */
+char *_strncat(char *dest, char*src, int n);
