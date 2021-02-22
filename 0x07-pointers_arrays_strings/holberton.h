01e9497ebba4421b7de9ef44688a97081dc6cc96
vasketz <anfevach@gmail.com>
2021-02-22 23:48:36 +0000

    Update header holberton.h

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -10,3 +10,7 @@ char *_memcpy(char *dest, char *src, unsigned int n);
  *This function locates a character in string.
  */
 char *_strchr(char *s, char c);
+/**
+ *This function gets the length of a prefix substring
+ */
+unsigned int _strspn(char *s, char *accept);
