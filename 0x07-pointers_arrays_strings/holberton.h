82ee96ea6335505141f6a6e7f089974032216a02
vasketz <anfevach@gmail.com>
2021-02-22 21:50:37 +0000

    Update header holberton.h

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -2,3 +2,7 @@
  *This function fills the memory with a byte
  */
 char *_memset(char *s, char b, unsigned int n);
+/**
+ *This functions copy a memory area.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n);
