f9cf3398d65c598f59b531a53b8f4ae644325433
vasketz <anfevach@gmail.com>
2021-02-22 22:15:38 +0000

    Update file 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strchr - finds a character in string
+ * @s: A pointer to the memory area to be found.
+ * @c: The character to find the memmory area with.
+ * 
+ * Return: a pointer to the filled memory area @s
+ */
+char *_strchr(char *s, char c)
+{
+	
+	for (;; s++)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		if (!*s) 
+		{
+			return (NULL);
+		}
+	}
+	return (NULL);
+}
