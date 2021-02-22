a156f615b170be5a84f7cdbc7778cb98f233c4b0
vasketz <anfevach@gmail.com>
2021-02-22 22:16:57 +0000

    Update file 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,19 +5,20 @@
  * _strchr - finds a character in string
  * @s: A pointer to the memory area to be found.
  * @c: The character to find the memmory area with.
- * 
+ *
  * Return: a pointer to the filled memory area @s
  */
 char *_strchr(char *s, char c)
 {
-	
+
 	for (;; s++)
 	{
 		if (*s == c)
 		{
 			return (s);
 		}
-		if (!*s) 
+
+		if (!*s)
 		{
 			return (NULL);
 		}
