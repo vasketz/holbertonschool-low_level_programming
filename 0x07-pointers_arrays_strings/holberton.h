92e92fe6f795fda441262736e535e63e5fff073b
vasketz <anfevach@gmail.com>
2021-02-22 22:15:04 +0000

    Update header holberton.h

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -6,3 +6,7 @@ char *_memset(char *s, char b, unsigned int n);
  *This functions copy a memory area.
  */
 char *_memcpy(char *dest, char *src, unsigned int n);
+/**
+ *This function locates a character in string.
+ */
+char *_strchr(char *s, char c);
