5a374544ffef42d554c66941d0952cd0be68302e
vasketz <anfevach@gmail.com>
2021-02-25 00:18:32 +0000

    Update header file holberton.h

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -6,3 +6,7 @@ int _putchar(char c);
  *This function prints the alphabet with recursion
  */
 void _puts_recursion(char *s);
+/**
+ *This function prints string in reverse with recursion
+ */
+void _print_rev_recursion(char *s);
