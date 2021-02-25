9345bc810c7145de1a26ac5b629bd5d5d5d81473
vasketz <anfevach@gmail.com>
2021-02-25 00:47:35 +0000

    Update file 4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion - check the code for Holberton School students.
+ *@x: is a variable
+ *@y: is a variable
+ * Return: Always 0.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	{
+		return (1);
+	}
+	else if (y < 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
