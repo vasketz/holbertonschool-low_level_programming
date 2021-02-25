6de703eef1679360f5a78fb7a1138b5159fec08d
vasketz <anfevach@gmail.com>
2021-02-25 00:20:34 +0000

    Update file 1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * _puts_recursion - check the code for Holberton School students.
+ * _print_rev_recursion - check the code for Holberton School students.
  *@s: is a pointer variable
  * Return: Always 0.
  */
