af4d4fa490a549b9a1d619dfb0d0a14a9c1734e2
vasketz <anfevach@gmail.com>
2021-02-25 00:28:59 +0000

    Update file 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _strlen_recursion - check the code for Holberton School students.
+ *@s: is a pointer variable
+ * Return: Always 0.
+ */
+int _strlen_recursion(char *s)
+{
+	if (s[0] == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
