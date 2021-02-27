51ba6139fc1ab8b34423b08007abf80bae36168e
vasketz <anfevach@gmail.com>
2021-02-27 22:13:13 +0000

    Update file print string in argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * whatsmynamename - prints its name followed by a new line
+ * @argc: is an argument of the main function.
+ * @argv: is an argument of the main function.
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+        int i;
+
+        for (i = 0; i < argc; i++)
+        {
+                printf("%s\n", argv[i]);
+        }
+        return (0);
+}
