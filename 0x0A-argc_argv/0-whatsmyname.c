7f6aa5eb4b3baa0bb4a933f3d25d10da639e576f
vasketz <anfevach@gmail.com>
2021-02-28 04:02:18 +0000

    Update file whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * whatsmynamename - prints its name followed by a new line
+ * main - prints its name followed by a new line
  * @argc: is an argument of the main function.
  * @argv: is an argument of the main function.
  * Return: Always 0.
@@ -10,11 +10,11 @@
 
 int main(int argc, char *argv[])
 {
-        int i;
+	int i;
 
-        for (i = 0; i < argc; i++)
-        {
-                printf("%s\n", argv[i]);
-        }
-        return (0);
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+		return (0);
 }
