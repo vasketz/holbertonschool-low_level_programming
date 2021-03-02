25cc657c24a395433f52d21ddfc43f7639719357
vasketz <anfevach@gmail.com>
2021-03-02 22:34:06 +0000

    Update file to return string

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 	char *ptr;
 	unsigned int i = 0;
 
-	ptr = malloc((size) * sizeof(*ptr));
+	ptr = malloc((size + 1) * sizeof(*ptr));
 	if (size == 0)
 	{
 		return (NULL);
