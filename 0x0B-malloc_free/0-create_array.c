aa7920b423e916e5be56f57a8e8de10dda6ed9a7
vasketz <anfevach@gmail.com>
2021-03-02 20:32:33 +0000

    Update file to return string

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - Create an array
+ * @c: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: string.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *ps;
+	unsigned int i;
+
+	ps = malloc((size + 1) * sizeof(char));
+	if (ps == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (i < size)
+	{
+		ps[i] = c;
+		i++;
+	}
+	ps[i] = '\0';
+	return (ps);
+}
