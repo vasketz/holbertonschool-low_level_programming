ec019e346c1933547cae5a6cda9e7a2ee3d3fc91
vasketz <anfevach@gmail.com>
2021-03-03 04:11:42 +0000

    Update header file

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -6,3 +6,7 @@ char *create_array(unsigned int size, char c);
  *This function returns pointer 
  */
 char *_strdup(char *str);
+/**
+ * This function concat two strings
+ */
+char *str_concat(char *s1, char *s2);
