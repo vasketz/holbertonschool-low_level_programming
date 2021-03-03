2f4baaae84881ac5af3e4468cfe3c200cabbb961
vasketz <anfevach@gmail.com>
2021-03-03 15:38:42 +0000

    Update file 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,15 +1,12 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 char *_strcpy(char *dest, char *src);
-
 /**
  * _strdup - Create an array
  * @str: the size of the memory to print
  * Return: string.
  */
-
 char *_strdup(char *str)
 {
 	char *ptr;
@@ -29,16 +26,12 @@ char *_strdup(char *str)
 		_strcpy(ptr, str);
 	return (ptr);
 }
-
 /**
  * _strcpy - Create an array
  * @dest: the address of memory to print
  * @src: the size of the memory to print
- *
  * Return: string.
  */
-
-
 char *_strcpy(char *dest, char *src)
 {
 	char *saved = dest;
