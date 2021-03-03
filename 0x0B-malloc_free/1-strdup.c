67ec3108c4c8daf51bd92d541edb8f569d4443cc
vasketz <anfevach@gmail.com>
2021-03-03 01:40:52 +0000

    Update file with copy of string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,12 +15,12 @@ char *_strdup(char *str)
 	char *ptr;
 	int i;
 
-	for (i = 0; i < *str + 1; i++)
-	{}
 	if (str == NULL)
 	{
 		return (NULL);
 	}
+	for (i = 0; i < *str + 1; i++)
+	{}
 	ptr = malloc((*str + 1) * sizeof(char));
 	if (ptr == 0)
 	{
