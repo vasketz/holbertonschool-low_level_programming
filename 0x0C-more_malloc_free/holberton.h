490f26fe1a8c526da999a097695aa715e34cfcee
vasketz <anfevach@gmail.com>
2021-03-05 04:37:56 +0000

    Update prototype holberton.h

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -2,3 +2,7 @@
  *This function allocated a space on memory 
  */
 void *malloc_checked(unsigned int b);
+/**
+ * This function concat two strings
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n);
