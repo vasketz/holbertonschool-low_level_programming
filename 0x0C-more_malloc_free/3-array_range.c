65dafa9d1e32fe1ad3e553220f60988cf71e0f5f
vasketz <anfevach@gmail.com>
2021-03-06 02:25:31 +0000

    Update file of task 3-array_range.c

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * array_range - create an array of strings
+ * @min: variable
+ * @max: variable
+ *
+ * Return: pointer.
+ */
+
+int *array_range(int min, int max)
+{
+	int *ptri;
+	int i;
+	int x;
+
+	if (min > max)
+	{
+		return (0);
+	}
+	for (i = min; i <= max; i++)
+	{}
+	ptri = malloc(sizeof(int) * i);
+	if (ptri == 0)
+	{
+		return (NULL);
+	}
+	for (x = 0; x <= max; x++)
+	{
+		ptri[x] = min++;
+	}
+	return (ptri);
+}
