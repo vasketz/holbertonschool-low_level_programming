4831336165ba766b9ca133c09a3b3666ec887a51
vasketz <anfevach@gmail.com>
2021-03-06 02:24:00 +0000

    Update header holberton.h

--- 0x0C-more_malloc_free/holberton.h
+++ 0x0C-more_malloc_free/holberton.h
@@ -10,3 +10,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
  * this function allocate memomry for an array, using malloc
  */
 void *_calloc(unsigned int nmemb, unsigned int size);
+/**
+ * This function create an array of entires;
+ */
+int *array_range(int min, int max);
