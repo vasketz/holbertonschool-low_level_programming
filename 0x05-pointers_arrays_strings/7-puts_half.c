d4c47cc7278891d96e19d916328f374dd0a86a1e
vasketz <anfevach@gmail.com>
2021-03-07 02:07:40 +0000

    Update file 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * puts2- check the code for Holberton School students.
+ *@str: is a pointer variable
+ * Return: Always 0.
+ */
+
+void puts_half(char *str)
+{
+	int x, y, z;
+
+	for (x = 0; str[x] != '\0'; x++)
+	{}
+	if (x % 2 == 0)
+	{
+		y = x / 2;
+
+		for (z = y; z < x; z++)
+		{
+			_putchar(str[z]);
+		}
+	}
+	else
+	{
+		y = (x - 1) / 2;
+
+		for (z = y; z < x; z++)
+		{
+			_putchar(str[z]);
+		}
+	}
+	_putchar('\n');
+}
