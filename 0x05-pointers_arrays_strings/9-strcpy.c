53a7ac4feaf1dbc46793503a341b672a38da1581
vasketz <anfevach@gmail.com>
2021-03-07 03:21:51 +0000

    Update file 9 strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strcpy- check the code for Holberton School students.
+ *@dest: is a pointer variable
+ *@src: is a source variable
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
