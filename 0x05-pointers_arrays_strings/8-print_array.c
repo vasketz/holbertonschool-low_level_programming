f87c837bb6dc5dad05c41758a4bc7d0627efe6e7
vasketz <anfevach@gmail.com>
2021-03-07 02:48:02 +0000

    Updae file 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_array - check the code for Holberton School students.
+ *@a: is a pointer
+ *@n: is a size
+ * Return: Always 0.
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d, ", *(a + i));
+	}
+	printf("\n");
+}
