3bee9dac7df2f494518c2606615a6ca9d117097c
vasketz <anfevach@gmail.com>
2021-03-09 02:48:38 +0000

    Update header dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,7 +13,7 @@ struct dog
 char *name;
 float age;
 char *owner;
-}; struct dog dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
