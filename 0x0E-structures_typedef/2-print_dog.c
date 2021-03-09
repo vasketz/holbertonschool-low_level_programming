51f9c459ed27b5e55aba5eedd3e62123454dead4
vasketz <anfevach@gmail.com>
2021-03-09 02:14:05 +0000

    Update file 2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - check the code for Holberton School students.
+ *@d: is pointer.
+ * Return: Always 0.
+ */
+void print_dog(struct dog *d)
+{
+	if (d == 0)
+	{
+
+	}
+	if (d->name != '\0')
+	{
+		printf("Name: %s\n", d->name);
+	}
+	else
+	{
+		printf("Name: %p\n", d->name);
+	}
+
+	if (d->age != '\0')
+	{
+		printf("Age: %f\n", d->age);
+	}
+	else
+	{
+		printf("Age: %p\n", (void *)&d->age);
+	}
+
+	if (d->owner != '\0')
+	{
+	printf("Owner: %s\n", d->owner);
+	}
+	else
+	{
+		printf("Owner: %p\n", d->owner);
+	}
+}
