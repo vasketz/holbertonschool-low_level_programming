29d270053076fb44a5645dea75efa29bbeb4bec7
vasketz <anfevach@gmail.com>
2021-03-10 17:06:00 +0000

    Update header

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,4 +1,9 @@
+#ifndef FUNCTION_POINTER_H
+#define FUNCTION_POINTER_H
 /**
- * This function print name 
+ * print_name - This function print name
+ * @name: is a pointer
+ * @f: is a pointer to function
  */
 void print_name(char *name, void (*f)(char *));
+#endif
