a2a0e419440f9f091c3e79a9ad8a38e1f751d25b
vasketz <anfevach@gmail.com>
2021-03-10 21:55:53 +0000

    Update header -h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -6,4 +6,12 @@
  * @f: is a pointer to function
  */
 void print_name(char *name, void (*f)(char *));
+/**
+ * array_iterator - print each element of array
+ *
+ * @array: array
+ * @size: size
+ * @action: action
+ */
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
