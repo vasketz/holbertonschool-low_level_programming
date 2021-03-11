4f7b4be71ff02fbb093fe4e2d08d3433a8c27bc3
vasketz <anfevach@gmail.com>
2021-03-11 22:53:18 +0000

    Update header variadic functions

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,10 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+/**
+ * sum_them_all - This function sum all numbers.
+ * @n: unsigned variable.
+ * Return: sum.
+ */
+int sum_them_all(const unsigned int n, ...);
+
+#endif
