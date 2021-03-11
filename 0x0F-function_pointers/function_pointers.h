1d9f552ba3c2aa92acf0493e895de32edfc62410
vasketz <anfevach@gmail.com>
2021-03-11 02:48:19 +0000

    Update header .h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
 #ifndef FUNCTION_POINTER_H
 #define FUNCTION_POINTER_H
 #include <stddef.h>
+#include <stdlib.h>
 /**
  * print_name - This function print name
  * @name: is a pointer
@@ -15,4 +16,13 @@ void print_name(char *name, void (*f)(char *));
  * @action: action
  */
 void array_iterator(int *array, size_t size, void (*action)(int));
+/**
+ * int_index - serach for a number
+ *
+ * @array: array pointer
+ * @size: size
+ * @cmp: cmp
+ * Return: int
+ */
+int int_index(int *array, int size, int (*cmp)(int));
 #endif
