d16209e499923c39f1669d89e1f61d528ac1130b
vasketz <anfevach@gmail.com>
2021-03-12 00:38:58 +0000

    Update file task 2

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,9 +8,16 @@
 int sum_them_all(const unsigned int n, ...);
 /**
  * print_numbers - of an array with variadic
- * @separator 
- * @n 
+ * @separator: print space 
+ * @n: print elements of array
  * Return: void 
  */
 void print_numbers(const char *separator, const unsigned int n, ...);
+/**
+ * print_numbers - of an array with variadic
+ * @separator: print space
+ * @n: elements of array
+ * Return: void 
+ */
+void print_strings(const char *separator, const unsigned int n, ...);
 #endif
