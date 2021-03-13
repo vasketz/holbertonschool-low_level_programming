2a1d992cdf347314fadc6eccac5272c648c0af6e
vasketz <anfevach@gmail.com>
2021-03-13 04:02:49 +0000

    Update main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -4,7 +4,7 @@
 
 /**
  * main - main  of program
- * @argc: number of args 
+ * @argc: number of args
  * @argv: string of args
  * Return: 0.
  */
@@ -22,7 +22,7 @@ int main(int argc, char *argv[])
 	num1 = atoi(argv[1]);
 	ptr = get_op_func(argv[2]);
 	num2 = atoi(argv[3]);
-	
+
 	if (ptr == NULL)
 	{
 		printf("Error\n");
