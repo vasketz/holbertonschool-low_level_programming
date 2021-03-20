1b13c83637d1f5664dd3f3d7af5a09870987c7d6
vasketz <anfevach@gmail.com>
2021-03-20 22:24:30 +0000

    Update task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point and execute the numbers with putchar
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+		if (x < '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
