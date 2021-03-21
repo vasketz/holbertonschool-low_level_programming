cb9ac240d9e0309f99b97bb5c3cd84ed273d35d0
vasketz <anfevach@gmail.com>
2021-03-21 05:27:32 +0000

    Update file task 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -9,13 +9,16 @@
 
 void print_number(int n)
 {
+	unsigned int u = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
+		u = -u;
 	}
-	if (n / 10 != 0)
+	if ((u / 10) != 0)
 	{
-		print_number(n / 10);
+		print_number(u / 10);
 	}
-	_putchar((n % 10) + '0');
+	_putchar((u % 10) + '0');
 }
