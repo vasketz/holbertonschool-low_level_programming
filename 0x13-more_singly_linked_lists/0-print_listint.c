259a71d0919320822bbdb8b3b7b70a68edbfff05
vasketz <anfevach@gmail.com>
2021-03-22 15:19:09 +0000

    New file task-0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_listint - print the elemest of the list
+ * 
+ * @h: is a pointer variable.
+ * Return: the number of elements
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int i;
+
+	if (h == NULL)
+	{
+		return (0);
+	}
+	for (i = 0; h != NULL; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (i);
+}
