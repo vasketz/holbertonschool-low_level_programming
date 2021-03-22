512facc960330046cf2c1ae9ea16c24c145a35e8
vasketz <anfevach@gmail.com>
2021-03-22 22:57:58 +0000

    New file task 100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 int print_(void)__attribute__((constructor));
 /**
- * main - check the code for Holberton School students.
+ * print_ - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 int print_(void)
 {
-    printf("You're beat! and yet, you must allow,\n");
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
-    return (0);
+	return (0);
 }
