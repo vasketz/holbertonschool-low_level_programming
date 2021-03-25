8a575953ca6c3415e9d96cd3f659636f1a4aea09
vasketz <anfevach@gmail.com>
2021-03-25 20:11:53 +0000

    Update file task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+int _strlen(char *s);
 /**
  * binary_to_uint - converts a binary string to unsigned int
  * @b: char pointer variable
@@ -11,7 +12,7 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int x = 1;
 	unsigned int y = 0;
 
-	lon = strlen(b);
+	lon = _strlen(b);
 
 	for (i = lon - 1; i >= 0; i--)
 	{
@@ -26,4 +27,20 @@ unsigned int binary_to_uint(const char *b)
 		x *= 2;
 	}
 	return (y);
+}
+/**
+ * _strlen - check the code for Holberton School students.
+ *@s: is a pointer variable
+ * Return: Always 0.
+ */
+int _strlen(const char *s)
+{
+	int len;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
 }
