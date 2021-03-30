2ddf832e4be4a3c5b2793c0309d24af873c35604
vasketz <anfevach@gmail.com>
2021-03-30 02:24:47 +0000

    Update file task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 		}
 	}
 	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-		if (file == -1)
+	if (file == -1)
 	{
 		return (0);
 	}
