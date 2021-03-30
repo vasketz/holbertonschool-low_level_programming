9c425aac73bf02e28691478f507e376c90692291
vasketz <anfevach@gmail.com>
2021-03-30 02:26:11 +0000

    Update file task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 	{
-		return (-1);
+		return (0);
 	}
 	if (text_content != NULL)
 	{
@@ -24,12 +24,12 @@ int create_file(const char *filename, char *text_content)
 	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (file == -1)
 	{
-		return (0);
+		return (-1);
 	}
 	print = write(file, text_content, sz);
 	if (print == -1)
 	{
-		return (0);
+		return (-1);
 	}
 	close(file);
 	return (1);
