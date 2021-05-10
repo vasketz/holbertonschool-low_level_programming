9ad19ea3a2a38b6d36d00c28211f7a07c9beb36a
vasketz <anfevach@gmail.com>
2021-05-10 03:20:22 +0000

    Update task 7

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,51 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - insert a new node a the given position
+ * @h: double pointer
+ * @idx: unsigned int
+ * @n: int
+ * Return: new node
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{	dlistint_t *tmp = *h, *new_n, *aux = *h;
+	unsigned int i = 0, j;
+
+	if (h == NULL)
+		return (NULL);
+	for (j = 0; aux != NULL; j++)
+		aux = aux->next;
+	if (idx > j - 1)
+		return (NULL);
+	new_n = malloc(sizeof(dlistint_t));
+	if (new_n == NULL)
+		return (NULL);
+	new_n->n = n;
+	if (idx == j)
+	{	new_n->prev = aux;
+		new_n->next = NULL;
+		aux->next = new_n;
+		return (new_n);
+	}
+	if (*h == NULL)
+	{	new_n->next = NULL;
+		new_n->prev = NULL;
+		*h = new_n;
+		return (new_n);
+	}
+	if (idx == 0)
+	{	new_n->next = NULL;
+		new_n->prev = NULL;
+		*h = new_n;
+		return (new_n);
+	}
+	while (i < idx)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+	new_n->next = tmp;
+	new_n->prev = tmp->prev;
+	tmp->prev->next = new_n;
+	tmp->prev = new_n;
+	return (new_n);
+}
