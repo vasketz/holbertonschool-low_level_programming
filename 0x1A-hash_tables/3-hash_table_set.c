621c2adb19c07f500eaaeecfaf2ee38aafb7e1f7
vasketz <anfevach@gmail.com>
2021-05-29 01:44:52 +0000

    Update task 3 set

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,16 +8,19 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index = key_index((const unsigned char *)key, ht->size);
-	hash_node_t *new = NULL;
+	unsigned long int index = 0;
+	hash_node_t *new = NULL, *tmp = NULL;
 
-	if (strcmp(key, "") == 0 || ht == NULL || key == NULL)
+	if (ht == NULL || key == NULL)
 		return (0);
+	if (strlen(key) == 0)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 		return (0);
-	new->key = strdup((char *)key);
-	new->value = strdup((char *)value);
+	new->key = strdup(key);
+	new->value = strdup(value);
 	if (ht->array[index] == NULL)
 	{
 		ht->array[index] = new;
@@ -26,6 +29,16 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	else
 	{
+		tmp = ht->array[index];
+		while (tmp != NULL)
+		{
+			if (strcmp(tmp->key, key) == 0)
+			{
+				tmp->value = strdup(value);
+				return (1);
+			}
+			tmp = tmp->next;
+		}
 		new->next = ht->array[index];
 		ht->array[index] = new;
 		return (1);
